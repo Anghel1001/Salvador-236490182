084dc259bd4fbaaa0b0475b6f984e1a304e490cd
Rhyzelle Salvador <142501341+Anghel1001@users.noreply.github.com>
2025-10-01 11:01:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trying Php</title>
+</head>
+<body>
+    <h1>Salvador, Angel Rhyzelle</h1>
+    <ul>
+        <li><a href="intro.php"> Introduce Yourself</a></li>
+        <li><a href="math.php"> Simple Math</a></li>
+        <li><a href="converter.php"> Temperature Coverter</a></li>
+        <li><a href="intro.php"> Introduce Yourself</a></li>
+    </ul>
+
+</body>
+</html>
